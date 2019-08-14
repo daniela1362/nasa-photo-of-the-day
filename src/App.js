1b3e6687ca904995534e82aeac6ad25a6f5c191e
daniela-arias-ordonez <daniela.arias2000@gmail.com>
2019-08-14 17:41:26 -0500

    Added video

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import "./App.css";
+import Video from "./components/nasa.js";
 
 function App() {
+const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=7TEedvatBtcZa95WqVIdCySPyv7Fed6GNGxpefyi&date=2012-03-14")
+    .then((info)=>{
+        console.log (info.data)
+  setData(info.data)
+  console.log (data);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+},
+[]);
+console.log (data)  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="shuttle">
+          <h1>NASA</h1>
+            <div>
+              {console.log(nasa)}
+              {<Video date={nasa.date} explanation={nasa.explanation} 
+              src={nasa.url} media_type={nasa.media_type} hdurl={nasa.hdurl}/> }
+            </div>
+        </div>
     </div>
   );
 }
